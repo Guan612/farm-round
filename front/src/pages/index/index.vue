6cd2701a23179d9ebe6af18c298e2b996391458a
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-07 11:02:50 +0800

    Update index.vue

--- front/src/pages/index/index.vue
+++ front/src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import tabbar from "@/compmnets/tabbar/tabbar.vue"
 
 const title = ref('Hello')
@@ -8,25 +8,35 @@ const title = ref('Hello')
 
 <template>
   <div class="flex flex-col">
-    <div>田皮鸭的家</div>
-    <div class="flex flex-row">
-      <div>全屋</div>
-      <div>客厅</div>
+    <div class="m-2 md:m-2 font-black">田皮鸭的家</div>
+    <div class="flex flex-row m-1 md:m-2 font-bold">
+      <div class="m-1">全屋</div>
+      <div class="m-1">客厅</div>
     </div>
-    <div class="flex flex-row">
-      <div class=" grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 ">
-        <div class="flex flex-col justify-center items-center">
-          <div class="flex flex-col ">
-            <div class="m-2">手机遥控器</div>
-            <up-switch v-model="value" @change="change" class="m-2"></up-switch>
-          </div>
-          <div class="flex flex-col">
-            <div class="m-2">手机遥控器</div>
-            <up-switch v-model="value" @change="change" class="m-2"></up-switch>
-          </div>
+    <div class="flex flex-row justify-around">
+      <div class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6">
+        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="m-2">手机遥控器</div>
+          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
+        </div>
+        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="m-2">xiaomi Su7</div>
+          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
+        </div>
+        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="m-2">电动牙刷</div>
+          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
+        </div>
+        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="m-2">空调</div>
+          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
+        </div>
+        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="m-2">温湿度计</div>
+          <div>温度：20摄氏度</div>
+          <div>湿度：50%</div>
         </div>
       </div>
-
     </div>
     <div class="m-10">
       <up-button type="primary" :plain="true" :hairline="true" text="编辑"></up-button>
