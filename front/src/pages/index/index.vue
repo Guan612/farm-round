bf1aa9f40ca6c866a7840fa69131668077f01b4d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-07 10:06:31 +0800

    修改主页

--- front/src/pages/index/index.vue
+++ front/src/pages/index/index.vue
@@ -8,8 +8,29 @@ const title = ref('Hello')
 
 <template>
   <div class="flex flex-col">
-    <div class="">区域1</div>
-    <div class="">区域2</div>
+    <div>田皮鸭的家</div>
+    <div class="flex flex-row">
+      <div>全屋</div>
+      <div>客厅</div>
+    </div>
+    <div class="flex flex-row">
+      <div class=" grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 ">
+        <div class="flex flex-col justify-center items-center">
+          <div class="flex flex-col ">
+            <div class="m-2">手机遥控器</div>
+            <up-switch v-model="value" @change="change" class="m-2"></up-switch>
+          </div>
+          <div class="flex flex-col">
+            <div class="m-2">手机遥控器</div>
+            <up-switch v-model="value" @change="change" class="m-2"></up-switch>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="m-10">
+      <up-button type="primary" :plain="true" :hairline="true" text="编辑"></up-button>
+    </div>
   </div>
   <tabbar active-tab-name="index"></tabbar>
 </template>
