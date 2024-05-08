69bc5829e12e89f8b43fbca1045bcbd05fe03a1c
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-08 09:02:16 +0800

    更新主页布局

--- front/src/pages/index/index.vue
+++ front/src/pages/index/index.vue
@@ -15,30 +15,31 @@ const title = ref('Hello')
     </div>
     <div class="flex flex-row justify-around">
       <div class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6">
-        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
-          <div class="m-2">手机遥控器</div>
+        <div class="m-1 p-2 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="w-20 h-20">
+            <img src="../../static/img/remoteControl.png" class="bg-auto w-full h-full">
+          </div>
+          <div class="font-bold">手机遥控器</div>
           <up-switch v-model="value" @change="change" class="m-2"></up-switch>
         </div>
-        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
-          <div class="m-2">xiaomi Su7</div>
-          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
-        </div>
-        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
-          <div class="m-2">电动牙刷</div>
-          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
-        </div>
-        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
-          <div class="m-2">空调</div>
-          <up-switch v-model="value" @change="change" class="m-2"></up-switch>
-        </div>
-        <div class="m-1 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
-          <div class="m-2">温湿度计</div>
+        <div class="m-1 p-2 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="w-20 h-20">
+            <img src="../../static/img/temprture.png" class="bg-auto w-full h-full">
+          </div>
+          <div class="font-bold">温湿度计</div>
           <div>温度：20摄氏度</div>
           <div>湿度：50%</div>
         </div>
+        <div class="m-1 p-2 flex flex-col bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg hover:shadow-2xl">
+          <div class="w-20 h-20">
+            <img src="../../static/img/spaker.png" class="bg-auto w-full h-full">
+          </div>
+          <div class="font-bold">蓝牙音箱</div>
+          <div></div>
+        </div>
       </div>
     </div>
-    <div class="m-10">
+    <div class="p-5">
       <up-button type="primary" :plain="true" :hairline="true" text="编辑"></up-button>
     </div>
   </div>
