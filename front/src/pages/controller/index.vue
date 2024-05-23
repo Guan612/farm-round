53f65627a44374cdb4b2d47e35535057272cd6c6
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-23 16:42:02 +0800

    Update index.vue

--- front/src/pages/controller/index.vue
+++ front/src/pages/controller/index.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 const list3 = ref([
     {
         image: 'https://cdn.uviewui.com/uview/swiper/swiper2.png',
-        title: '昨夜星辰昨夜风，画楼西畔桂堂东',
+        title: '百合子我老婆',
     },
     {
         image: 'https://cdn.uviewui.com/uview/swiper/swiper1.png',
@@ -15,9 +15,29 @@ const list3 = ref([
         title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳',
     },
 ]);
+
+const title = ref('标题');
+const list = ref([
+    {
+        name: '选项一',
+        subname: "选项一描述",
+        color: '#ffaa7f',
+        fontSize: '20'
+    },
+    {
+        name: '选项二禁用',
+        disabled: true
+    },
+    {
+        name: '开启load加载', // 开启后文字不显示  
+        loading: true
+    }
+]);
+const show = ref(true); 
 </script>
 
 <template>
+    <up-action-sheet :show="show" @close="show = false" :actions="list" cancelText="取消"></up-action-sheet>
     <div class="flex flex-col">
         <up-swiper :list="list3" previousMargin="30" nextMargin="30" circular :autoplay="false" radius="5" indicator
             keyName="image" showTitle indicatorMode="line" bgColor="#ffffff" class="m-2"></up-swiper>
@@ -33,7 +53,7 @@ const list3 = ref([
         </div>
         <div
             class="flex flex-row m-2 p-3 justify-between items-center bg-gradient-to-r from-sky-500 to-indigo-500 rounded-2xl">
-            <div class="pl-3">
+            <div class="pl-3" @click="show = true">
                 <div class="font-extrabold">添加设备</div>
                 <div class="text-sm">添加设备</div>
             </div>
