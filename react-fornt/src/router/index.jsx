9e90c81ca253012cb8550e48e54318c73c388794
Guan612 <tiantony53@163.com>
2024-05-28 21:45:22 +0800

    优化路由

--- react-fornt/src/router/index.jsx
+++ react-fornt/src/router/index.jsx
@@ -1,35 +1,40 @@
-import {useRoutes, Navigate} from 'react-router-dom'
+import { useRoutes, Navigate } from "react-router-dom";
 
-import Layout from '../layout/indexlayout'
-import Home from '../pages/home'
-import Me from '../pages/me'
-import Equipment from '../pages/equipment'
+import Layout from "../layout/indexlayout";
+import Home from "../pages/home";
+import Me from "../pages/me";
+import Equipment from "../pages/equipment";
+import AddEqu from "../pages/equipment/addequ";
 
 const routers = [
     {
-        path: '*',
+        path: "*",
         element: <Navigate to="/layout/home" />,
     },
     {
-        path: '/layout',
-        element: <Layout/>,
-        children:[
+        path: "/layout",
+        element: <Layout />,
+        children: [
             {
-                path: 'home',
-                element: <Home/>,
+                path: "home",
+                element: <Home />,
             },
             {
-                path: 'me',
-                element: <Me/>,
+                path: "me",
+                element: <Me />,
             },
             {
-                path: 'equipment',
-                element: <Equipment/>,
+                path: "equipment",
+                element: <Equipment />,
             },
-        ]
+        ],
     },
-]
+    {
+        path: "/addequ",
+        element: <AddEqu />,
+    },
+];
 
 export default function Router() {
-    return useRoutes(routers)
+    return useRoutes(routers);
 }
