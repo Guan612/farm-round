82596196660124d15045b8363f93602983bce516
Guan612 <tiantony53@163.com>
2024-05-28 21:45:31 +0800

    本地存储

--- react-fornt/src/store/iotipStore.js
+++ react-fornt/src/store/iotipStore.js
@@ -1,10 +1,18 @@
-import {create} from 'zustand'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useIotInfoStore = create((set) => ({
-    iotInfo: {
-        name: '',
-        ip: '',
-    },
-    setIotInfo: (iotInfo) => set(() => ({ iotInfo })),
-    // ... other state and actions
-}))
+const useIotInfoStore = create(
+    persist(
+        (set) => ({
+            iotInfo: {},
+            setIotInfo: (iotInfo) => set(() => ({ iotInfo })),
+            clearIotInfo: () => set(() => ({ iotInfo: {} })),
+        }),
+        {
+            name: "iot-info-storage", // 本地存储的键
+            getStorage: () => localStorage, // 使用 localStorage
+        }
+    )
+);
+
+export default useIotInfoStore;
