3ae272a20fd88910e759c4c76844e7bb253f8cc0
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-29 09:27:55 +0800

    更新刷新逻辑

--- react-fornt/src/pages/equipment/addequ.jsx
+++ react-fornt/src/pages/equipment/addequ.jsx
@@ -1,32 +1,61 @@
+import { useNavigate } from "react-router-dom";
 import { Button, Input, Form } from "react-vant";
+import { ArrowLeft } from "@react-vant/icons";
 import useIotInfoStore from "../../store/iotipStore";
+import { useEffect } from "react";
 export default function AddEqu() {
-    const { setIotInfo, clearIotInfo } = useIotInfoStore.getState();
+    const navigate = useNavigate();
+    const { iotInfo, setIotInfo, clearIotInfo } = useIotInfoStore.getState();
     const [form] = Form.useForm();
 
     const onFinish = (values) => {
         console.log(values);
         setIotInfo(values);
+        navigate('/addequ', { replace: true });
     };
-
+    
     return (
         <div className="flex flex-col bg-gray-900 text-white h-screen">
+            <div className="flex flex-row items-center m-2">
+                <Button
+                    icon={<ArrowLeft />}
+                    size="small"
+                    round
+                    onClick={() => navigate(-1)}
+                ></Button>
+                <span className="text-xl ml-2">添加设备</span>
+            </div>
             <div className="m-2">
                 <Form
                     form={form}
                     onFinish={onFinish}
                     footer={
                         <div style={{ margin: "16px 16px 0" }}>
-                            <Button 
+                            <Button
                                 className="mb-2"
                                 round
                                 nativeType="submit"
                                 type="primary"
                                 block
                             >
-                                提交
+                                {iotInfo.equip ? "更新设备" : "添加设备"}
                             </Button>
-                            <Button round onClick={clearIotInfo}>清除iot信息</Button>
+                            <div className="flex flex-row justify-around">
+                                {iotInfo.equip && (
+                                    <>
+                                        <Button round type="info">
+                                            查看iot信息
+                                        </Button>
+                                        <Button
+                                            round
+                                            type="warning"
+                                            onClick={clearIotInfo}
+                                        >
+                                            删除iot信息
+                                        </Button>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     }
                 >
--- react-fornt/src/store/iotipStore.js
+++ react-fornt/src/store/iotipStore.js
@@ -1,16 +1,25 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { useNavigate } from "react-router-dom";
 
 const useIotInfoStore = create(
     persist(
         (set) => ({
             iotInfo: {},
-            setIotInfo: (iotInfo) => set(() => ({ iotInfo })),
-            clearIotInfo: () => set(() => ({ iotInfo: {} })),
+            setIotInfo: (iotInfo) => {
+                set(() => ({ iotInfo }));
+
+            },
+            clearIotInfo: () => {
+                set(() => ({ iotInfo: {} }));
+
+            },
         }),
         {
             name: "iot-info-storage", // 本地存储的键
-            getStorage: () => localStorage, // 使用 localStorage
+            storage: localStorage, // 使用 localStorage
+            serialize: JSON.stringify, // 指定序列化函数
+            deserialize: JSON.parse, // 指定反序列化函数
         }
     )
 );
