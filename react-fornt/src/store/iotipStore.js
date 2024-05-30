7bce79efb16d5d24425da4e1593ea0d59ad35e8f
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-30 08:21:47 +0800

    更新存储逻辑

--- react-fornt/src/store/iotipStore.js
+++ react-fornt/src/store/iotipStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { useNavigate } from "react-router-dom";
 
 const useIotInfoStore = create(
@@ -17,9 +17,7 @@ const useIotInfoStore = create(
         }),
         {
             name: "iot-info-storage", // 本地存储的键
-            storage: localStorage, // 使用 localStorage
-            serialize: JSON.stringify, // 指定序列化函数
-            deserialize: JSON.parse, // 指定反序列化函数
+            storage: createJSONStorage(() => localStorage)
         }
     )
 );
